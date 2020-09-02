10a09444641f6bb9f4d4422cbb0d252ba316ae28
Samir Sigh <samirsinghpat82@gmail.com>
2020-09-02 20:26:43 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
+//import logo from './components/erricsion.png';
+import  NewsCards from './components/NewsCards/NewsCards';
+//import Modal from './components/Modal/Modal';
+import useStyles from './styles';
+
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: 'f105c8026d64302d1f8f319d71e22ed52e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          }
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src="https://www.indiafellow.org/blog/wp-content/uploads/2020/05/Marketplace-Lending-News.jpg" className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      
     </div>
   );
-}
+};
 
 export default App;
+
