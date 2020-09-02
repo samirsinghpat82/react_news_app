da468e0e165f3f825c367e36e962cc06324e979c
Samir Sigh <samirsinghpat82@gmail.com>
2020-09-02 20:36:11 +0530

    Updates

---
+++ precache-manifest.6adb28d4dacd3a3f8353712d571c196f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11264a622733165d0333a6ed3145b3ee",
+    "url": "/react_news_app/index.html"
+  },
+  {
+    "revision": "94e591e4f73e3676a8ef",
+    "url": "/react_news_app/static/css/main.90a9bc0a.chunk.css"
+  },
+  {
+    "revision": "4e53508199232a0bdac9",
+    "url": "/react_news_app/static/js/2.5a756921.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react_news_app/static/js/2.5a756921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94e591e4f73e3676a8ef",
+    "url": "/react_news_app/static/js/main.3ac1cb31.chunk.js"
+  },
+  {
+    "revision": "e53142de469526a4faa9",
+    "url": "/react_news_app/static/js/runtime-main.bffd0e16.js"
+  }
+]);
